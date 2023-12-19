512201d0cb00b86b277de53c78dbcf3d02de61cb
fl42v <fl42v@proton.me>
2023-12-19 08:15:12 +0300

    re-comment stuff, since it doesn't seem to change anything

--- devices/families/sdm845-mainline/kernel/default.nix
+++ devices/families/sdm845-mainline/kernel/default.nix
@@ -15,15 +15,6 @@ mobile-nixos.kernel-builder {
     hash = "sha256-StE6pFwSPklhI0xjp85JSPG0yIFOZ6VU72mQoVrIFSo=";
   };
 
-  # still necessary?
-  #patches = [
-  #  # ASoC: codecs: tas2559: Fix build
-  #  (fetchpatch {
-  #    url = "https://github.com/samueldr/linux/commit/d1b59edd94153ac153043fb038ccc4e6c1384009.patch";
-  #    sha256 = "sha256-zu1m+WNHPoXv3VnbW16R9SwKQzMYnwYEUdp35kUSKoE=";
-  #  })
-  #];
-
   isModular = false;
   isCompressed = "gz";
 }
